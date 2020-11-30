11c4958d551ddba5fd6554629f7f60c7550529b3
cesar.carneiro <cesar.carneiro2@ufla.br>
2020-11-29 21:26:54 -0300

    Start projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    AlunosComponent,
+      ProfessoresComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
